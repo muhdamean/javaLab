96eff33a35abbd7822fb2517f7dab931d7ea6288
muhdamean <muhdaliyu1@gmail.com>
2019-05-04 06:53:09 +0100

    Java Lab Activity, U15/FNS/CSC/038

---
+++ LAB01/PatternDisplay.java
@@ -0,0 +1,10 @@
+//program to display a given pattern
+//U15/FNS/CSC/038
+public class PatternDisplay {
+	public static void main(String[] args) {
+		System.out.println("\t*\n\n   *\t     *\n\n*\t*\t*\n\n*\t\t*");
+		System.out.println("\n\n\n*\t*\n\n*\t*\t*\n\n*\t*\n\n*\t*\t*\n\n*\t*");
+		System.out.println("\n\n\n*\t\t*\n\n*\t\t*\n\n*\t\t*\n\n*\t\t*\n\n    * \t   *");
+	}
+
+}
---
+++ LAB03/ClosedBox.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class closedBox here.
+ *
+ * LAB03 
+ * ALIYU MUHAMMAD AMINU
+ * U15/FNS/CSC/038
+ */
+import java.util.Scanner;
+public class ClosedBox
+{
+    public static void main(String[] args){
+        Scanner input =new Scanner(System.in);
+        double length, width, height,volume,area;
+        System.out.println("Enter the length: ");
+        length=input.nextDouble();
+        System.out.println("Enter the width: ");
+        width=input.nextDouble();
+        System.out.println("Enter the height: ");
+        height=input.nextDouble();
+        //Calculating the volume of the box
+        volume=height * width * length;
+        //calculating the surface area of the box
+        area= 2*(height* width)+ 2*(height* length)+2*(width*length);
+        //printing the result
+        System.out.println("The Volume of the box =   "+volume);
+        System.out.println("The Surface area of the box =   "+area);
+    }
+}
